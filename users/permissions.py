b95ab946416b08fbc3c540baf024c63b6eb259f7
Artem <artemradckevich@gmail.com>
2026-06-09 12:09:42 +0300

    Resolved permission validation and added API schema support

--- users/permissions.py
+++ users/permissions.py
@@ -5,4 +5,8 @@ class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
-        return obj.author.user == request.user
+        if hasattr(obj, "author") and hasattr(obj.author, "user"):
+            return obj.author.user == request.user
+        if hasattr(obj, "user"):
+            return obj.user == request.user
+        return False
