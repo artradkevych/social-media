80df67cb3cb2e9ee0b74526270044df8be633cbc
Artem <artemradckevich@gmail.com>
2026-06-09 11:44:56 +0300

    Registered User and Profile models in Django admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext as _
 
-# Register your models here.
+from users.models import User, Profile
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "username", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "first_name",
+        "last_name",
+        "location",
+    )
+    search_fields = (
+        "first_name",
+        "last_name",
+        "user__username",
+        "user__email",
+        "location",
+    )
+    list_select_related = ("user",)
+    list_filter = ("location",)
+    ordering = ("-id",)
+    filter_horizontal = ("following",)
